0ef7ea9be845b9e27977fb14b36c5614681d9e32
Donny Davis <donnywdavis@icloud.com>
2016-08-06 17:56:04 -0400

    Create struct for State object

---
+++ USStatePicker/Classes/State.swift
@@ -0,0 +1,19 @@
+//
+//  State.swift
+//  Pods
+//
+//  Created by Donny Davis on 8/6/16.
+//
+//
+
+import UIKit
+
+public struct State {
+    var code: String?
+    var name: String?
+    
+    init(code: String, name: String) {
+        self.code = code
+        self.name = name
+    }
+}
