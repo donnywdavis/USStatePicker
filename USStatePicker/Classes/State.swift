36a606e9048d1a280fddf320365a61d88f8aff06
Donny Davis <donnywdavis@icloud.com>
2016-08-06 21:05:35 -0400

    Make the properties for the State struct public so that they can be referenced in the delegate methods properly

--- USStatePicker/Classes/State.swift
+++ USStatePicker/Classes/State.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 public struct State {
-    var code: String?
-    var name: String?
+    public var code: String?
+    public var name: String?
     
     init(code: String, name: String) {
         self.code = code
